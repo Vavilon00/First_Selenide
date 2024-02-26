aead3da5488fcab15987424bb0b0dfced2203220
DD <DD@Asus>
2024-02-26 12:08:31 +0300

    add allure

--- src/test/java/Lesson1.java
+++ src/test/java/Lesson1.java
@@ -1,4 +1,7 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -13,5 +16,15 @@ public class Lesson1 {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+    @DisplayName("неуспешный тест")
+    @Test
+    void unsuccessfulSearchTest() {
+        SelenideLogger.addListener("allure",new AllureSelenide());
+        Configuration.holdBrowserOpen=true;
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("unsuccessfulSearchTest"));
     }
 }
