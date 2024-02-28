20de9e7f5f1c7ad5b906934f0371b70913c4565f
DD <DD@Asus>
2024-02-28 14:49:42 +0300

    update PageTextBoxTest

--- src/test/java/com/demoqa/PageTextBoxTest.java
+++ src/test/java/com/demoqa/PageTextBoxTest.java
@@ -15,16 +15,15 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class PageTextBoxTest {
     @BeforeAll
     public static void setUp(){
-//        Configuration.headless = true;
+       Configuration.headless = true;
         Configuration.pageLoadTimeout=60000;
         SelenideLogger.addListener("allure",new AllureSelenide());
     }
     @Test
     @Tag("UI")
     void successfulTest()  {
-        Configuration.holdBrowserOpen=true;
+//        Configuration.holdBrowserOpen=true;
  //       Configuration.browserSize = "1920x1080";
-
         PageTextBox pageTextBox = new PageTextBox();
         pageTextBox.openPage();
         pageTextBox.setFullName("setFullName");
